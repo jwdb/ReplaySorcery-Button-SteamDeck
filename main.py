087837885f068df09511d3950049d4f6b7722528
Jan-Willem <git@jan-willem.dev>
2022-06-03 23:40:48 +0200

    Feature: Call subprocess recordsorcery

--- main.py
+++ main.py
@@ -1,11 +1,9 @@
 class Plugin:
+    import subprocess
     # A normal method. It can be called from JavaScript using call_plugin_function("method_1", argument1, argument2)
-    async def method_1(self, *args):
-        pass
+    async def RecordSorcerySave(self, *args):
+        subprocess.run(["replay-sorcery", "save"])
 
-    # A normal method. It can be called from JavaScript using call_plugin_function("method_2", argument1, argument2)
-    async def method_2(self, *args):
-        pass
 
     # Asyncio-compatible long-running code, executed in a task when the plugin is loaded
     async def _main(self):
