79e6373afcb5f4699a9f162d0346e1c2dc8d2263
Jan-Willem <git@jan-willem.dev>
2022-06-03 23:59:15 +0200

    Fix: Call using OS and js differently

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 class Plugin:
-    import subprocess
     # A normal method. It can be called from JavaScript using call_plugin_function("method_1", argument1, argument2)
     async def RecordSorcerySave(self, *args):
-        subprocess.run(["replay-sorcery", "save"])
+        import os
+        os.system("replay-sorcery save")
 
 
     # Asyncio-compatible long-running code, executed in a task when the plugin is loaded
